6165df32e0dd25d764cfba5a1d10833ecfe83797
Cristian335-creaty <gilcarocristianfernando@gmail.com>
2025-09-19 13:33:59 -0300

    Proyecto base + workflow Horusec

---
+++ tp2-calidad1/src/main/java/com/ejemplo1/tp2-calidad1/App.java
@@ -0,0 +1,10 @@
+package com.ejemplo1.tp2-calidad1;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
